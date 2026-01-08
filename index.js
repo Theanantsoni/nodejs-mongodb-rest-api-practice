6704ddce1176d8c8cccdea6b04491fb076a6cacd
Soni Anant Rajeshkumar <137305850+Theanantsoni@users.noreply.github.com>
2026-01-08 17:50:08 +0530

    Add instructions for importing concept apps
    Added notes for importing concepts in index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// NOTE:
+
+// Uncomment the import of the concept you want to run.
+// Keep all other imports commented.
+// Only ONE app.js should be active at a time to avoid port conflicts.
+
+// --------------------------------------------------
+
 // import "./concepts/01-connect-mongodb/app.js";
 // import "./concepts/02-display-ui-ejs/app.js";
 // import "./concepts/03-make-api/app.js";
